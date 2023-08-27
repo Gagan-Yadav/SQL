952b45fc7b2181f207044c413bbbb4b8c8bd9f85
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-08-27 21:24:45 +0530

    Update

--- SQL Basic Commands.sql
+++ SQL Basic Commands.sql
@@ -2,13 +2,13 @@
 CREATE DATABASE MyDatabase;
 
 -- Use the following syntax to replace the current database with another database you’re working on:
-USE db_name;
+USE MyDatabase;
 
 -- Using the following syntax, we can permanently destroy a database and its associated files. All the tables that are a part of that database will be deleted if we use the following command.
-DROP DATABASE IF EXISTS db_name;  
+DROP DATABASE IF EXISTS MyDatabase;  
 
 -- Use the following syntax to display all databases on the current server:
-mysql> SHOW DATABASES; 
+SHOW DATABASES; 
 
 -- This command displays all tables in a current database.
 SHOW TABLES;
@@ -21,8 +21,7 @@ CREATE TABLE Marks(
       );
 
 -- To insert values into a table type the following command:
-INSERT INTO Marks
-VALUES (001,'GAGAN',94);
+INSERT INTO Marks VALUES (001,'GAGAN',94);
 
 -- In a table, add a new column:
 -- For example to add Subject Name column, you have to type the following command:

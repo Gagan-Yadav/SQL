75af7eb92827f9a48f8b27717e95db75d1faf9c8
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-08-29 21:16:05 +0530

    Add

---
+++ Clauses in SQL.sql
@@ -0,0 +1,6 @@
+-- A clause in SQL is a built-in function that helps to fetch the required records from a database table. A clause receives a 
+-- conditional expression, i.e. a column name or some terms involving the columns. The clause calculates the result based on the 
+-- given statements in the expression. When a large amount of data is stored in the database, clauses are helpful to filter and 
+-- analyze the queries. There are different types of clauses in SQL that are available for fetching the desired data.
+
+-- HAVING =>HAVING clause can be used in a GROUP BY clause. It is used to specify a search condition for a group in the database tables.

d870e9ba75b8ce1573af9f52c29aa6ae88468d4c
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-08-29 21:21:47 +0530

    Add

--- Clauses in SQL.sql
+++ Clauses in SQL.sql
@@ -4,3 +4,18 @@
 -- analyze the queries. There are different types of clauses in SQL that are available for fetching the desired data.
 
 -- HAVING =>HAVING clause can be used in a GROUP BY clause. It is used to specify a search condition for a group in the database tables.
+
+-- WHERE =>	The WHERE clause in SQL is used to retrieve the specific data from the database that specifies the conditions exactly that are given in the UPDATE, DELETE, etc. statements.
+
+-- Students Reference Table
+
+-- stu_id	stu_name	    stu_fees	stu_subject	    stu_age	    stu_class
+-- 1	    Divyesha Patil	3000	    Maths	            16	    10
+-- 2	    Mayra Pandit	2000	    Social Science	    15	    10
+-- 3	    Kunal Purohit	4500	    Chemistry	        17	    11
+-- 4	    Manvi Tyagi	    2000	    Social Science	    16	    9
+-- 5	    Joy Yadav	    3000	    Maths	            16	    9
+-- 6	    Tisha Shah	    2500	    Science	            15	    9
+-- 7	    Surbhi Soni	    4000	    Chemistry	        17	    10
+
+SELECT * FROM Students WHERE stu_fees < 3500;

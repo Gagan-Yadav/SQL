45ca668f792a73f6d1e6066ae4709d47f51b78a0
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-10 13:31:05 +0530

    Initial

--- Joins.sql
+++ Joins.sql
@@ -3,6 +3,25 @@
 -- Here are the different types of the JOINs in SQL:
 
 -- (INNER) JOIN :  Returns records that have matching values in both tables
--- LEFT (OUTER) JOIN :  Returns all records from the left table, and the matched records from the right table
--- RIGHT (OUTER) JOIN :  Returns all records from the right table, and the matched records from the left table
+
+SELECT column_name(s) FROM table1 INNER JOIN table2 ON table1.column_name = table2.column_name;
+-- Example
+SELECT Orders.OrderID, Customers.CustomerName FROM Orders INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID;
+
+
+-- LEFT (OUTER) JOIN :  
+-- The LEFT JOIN keyword returns all records from the left table (table1), and the matching records from the right table (table2).
+-- The result is 0 records from the right side, if there is no match.
+
+SELECT column_name(s) FROM table1 LEFT JOIN table2 ON table1.column_name = table2.column_name;
+-- Example
+SELECT Customers.CustomerName, Orders.OrderID FROM Customers LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID ORDER BY Customers.CustomerName;
+
+
+-- RIGHT (OUTER) JOIN :  
+-- The RIGHT JOIN keyword returns all records from the right table (table2), and the matching records from the left table (table1).
+-- The result is 0 records from the left side, if there is no match.
+
+SELECT column_name(s) FROM table1 RIGHT JOIN table2 ON table1.column_name = table2.column_name;
+
 -- FULL (OUTER) JOIN :  Returns all records when there is a match in either left or right table

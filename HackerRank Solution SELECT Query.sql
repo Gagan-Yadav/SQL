b40a50886ec9c8e3ee58aa95d80d63eecc341251
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-24 11:37:05 +0530

    int

--- HackerRank Solution SELECT Query.sql
+++ HackerRank Solution SELECT Query.sql
@@ -20,3 +20,6 @@ SELECT DISTINCT CITY FROM STATION WHERE upper(SUBSTR(CITY,1,1)) NOT IN ('A','E',
 
 -- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
 SELECT DISTINCT CITY FROM STATION WHERE UPPER(SUBSTR(CITY, LENGTH(CITY), 1)) NOT IN ('A','E','I','O','U') AND LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');    
+
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') OR LOWER(SUBSTR(CITY, LENGTH(CITY),1)) NOT IN ('a','e','i','o','u');   

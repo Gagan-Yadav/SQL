688f0dead15fe64143717b556d44dc962eeca9f1
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-24 11:44:06 +0530

    Initial commit

--- HackerRank Solution SELECT Query.sql
+++ HackerRank Solution SELECT Query.sql
@@ -32,3 +32,6 @@ SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY SUBSTR(NAME, LENGTH(NAME)-2,
 
 -- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
 SELECT NAME FROM EMPLOYEE ORDER BY NAME;   
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in Employee having a salary greater than  per month who have been employees for less than  months. Sort your result by ascending employee_id.
+SELECT NAME FROM EMPLOYEE WHERE SALARY > 2000  AND MONTHS < 10 ORDER BY EMPLOYEE_ID;  

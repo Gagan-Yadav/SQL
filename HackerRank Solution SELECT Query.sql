8130f6e9f0d10565de92be7b20de9bea6dca6a8e
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-24 11:42:47 +0530

    int

--- HackerRank Solution SELECT Query.sql
+++ HackerRank Solution SELECT Query.sql
@@ -29,3 +29,6 @@ SELECT DISTINCT CITY FROM STATION WHERE LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e',
 
 -- Query the Name of any student in STUDENTS who scored higher than  Marks. Order your output by the last three characters of each name. If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.), secondary sort them by ascending ID
 SELECT NAME FROM STUDENTS WHERE MARKS > 75 ORDER BY SUBSTR(NAME, LENGTH(NAME)-2, 3), ID;    
+
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+SELECT NAME FROM EMPLOYEE ORDER BY NAME;   

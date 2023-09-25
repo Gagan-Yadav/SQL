23d7d0e18651714fa8b6482494d7776a7f147227
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-25 13:34:19 +0530

    in

--- Aggregation.sql
+++ Aggregation.sql
@@ -3,3 +3,6 @@ SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = "California";
 
 -- Query the average population of all cities in CITY where District is California.
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = "California";
+
+-- Query the average population for all cities in CITY, rounded down to the nearest integer.
+SELECT FLOOR(AVG(Population)) FROM City;

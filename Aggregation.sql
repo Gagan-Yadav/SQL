23a91d9079baee876627f55aff46727b0f0ffee9
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-25 13:32:34 +0530

    in

--- Aggregation.sql
+++ Aggregation.sql
@@ -1,2 +1,5 @@
 -- Query the total population of all cities in CITY where District is California.
 SELECT SUM(POPULATION) FROM CITY WHERE DISTRICT = "California";
+
+-- Query the average population of all cities in CITY where District is California.
+SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = "California";

73ace5d5d8f8ae5f8204fcb168ed2dff72e7a275
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-25 13:49:40 +0530

    int

--- Aggregation.sql
+++ Aggregation.sql
@@ -26,3 +26,6 @@ SELECT ROUND(SUM(LAT_N), 2), ROUND(SUM(LONG_W), 2) FROM STATION;
 
 -- Write a query to find the sum of the Northern Latitudes having values greater than 38.7880 and less than 137.2345 up to 4 decimal places.
 SELECT ROUND(SUM(LAT_N),4) FROM STATION WHERE LAT_N BETWEEN 38.7880 AND 137.2345;
+
+-- Write a query to find the greatest value of the Northern Latitudes less than 137.2345 up to 4 decimal places.
+SELECT ROUND(MAX(LAT_N),4) FROM STATION WHERE LAT_N < 137.2345;

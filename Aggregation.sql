416f56693a4bc318e9bc00b43a045dcfe4f7256a
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-25 13:36:21 +0530

    in

--- Aggregation.sql
+++ Aggregation.sql
@@ -6,3 +6,6 @@ SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = "California";
 
 -- Query the average population for all cities in CITY, rounded down to the nearest integer.
 SELECT FLOOR(AVG(Population)) FROM City;
+
+-- Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN.
+SELECT SUM(Population) FROM City WHERE CountryCode='JPN';

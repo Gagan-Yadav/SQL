ca63cfa0407a2313c55084c40f76e1b83cdece3b
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-26 11:44:24 +0530

    IN

--- Aggregation.sql
+++ Aggregation.sql
@@ -32,3 +32,6 @@ SELECT ROUND(MAX(LAT_N),4) FROM STATION WHERE LAT_N < 137.2345;
 
 -- Write a query to find the corresponding Western Longitude value for the greatest value of the Northern Latitudes less than 137.2345 up to 4 decimal places.
 SELECT ROUND(LONG_W,4) FROM STATION WHERE LAT_N = (SELECT MAX(LAT_N) FROM STATION WHERE LAT_N < 137.2345);
+
+-- Query a count of the number of cities in CITY having a Population larger than 100,000.
+SELECT count(NAME) FROM CITY WHERE POPULATION > 100000;

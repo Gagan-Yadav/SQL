5c0bc51113317941cdc576c5691526efea6fad1a
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-27 12:54:43 +0530

    in

--- Joins HackerRank.sql
+++ Joins HackerRank.sql
@@ -1,3 +1,7 @@
 -- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 SELECT Country.Continent, FLOOR(AVG(City.Population)) FROM Country, City  WHERE Country.Code = City.CountryCode  GROUP BY Country.Continent ;
+
+-- Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+SELECT SUM(City.Population) FROM City, Country WHERE City.CountryCode = Country.Code AND Continent = 'Asia';

93c1de43a119ffe3003f51c30dd9edb1dbb2f1e6
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-27 12:45:39 +0530

    in

--- Joins HackerRank.sql
+++ Joins HackerRank.sql
@@ -1,3 +1,3 @@
-
-
+-- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+-- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 SELECT Country.Continent, FLOOR(AVG(City.Population)) FROM Country, City  WHERE Country.Code = City.CountryCode  GROUP BY Country.Continent ;

2099a30e44a777018740a5c4f1b931b00b53def5
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-09-27 12:27:53 +0530

    in

--- Draw the triangle HackerRank.sql
+++ Draw the triangle HackerRank.sql
@@ -1,7 +1,3 @@
-SELECT CASE
-WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
-WHEN A = B AND B = C THEN 'Equilateral'
-WHEN A = B OR B = C OR A = C THEN 'Isosceles'
-ELSE 'Scalene'
-END
-FROM TRIANGLES;
+
+
+SELECT CASE WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle' WHEN A = B AND B = C THEN 'Equilateral' WHEN A = B OR B = C OR A = C THEN 'Isosceles' ELSE 'Scalene' END FROM TRIANGLES;

597a1a74bc391a91fb8a331200134b1199982207
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-12-17 13:49:19 +0530

    a

--- Complete SQL.sql
+++ Complete SQL.sql
@@ -55,3 +55,57 @@ DQL
 • UPDATE
 
 • DELETE
+
+6. Data Definition • CREATE
+
+• ALTER • DROP
+
+• Indexes
+
+7.Query Filtering and Sorting
+
+• WHERE
+
+• AND
+
+• OR Conditions
+
+• Ascending
+
+• Descending
+
+8. Data Aggregation
+
+• SUM
+
+• AVG
+
+• COUNT
+
+• MIN
+
+• MAX
+
+9.Joins and Relationships
+
+• INNER JOIN
+
+• LEFT JOIN
+
+• RIGHT JOIN
+
+• Self-Joins
+
+• Cross Joins
+
+• FULL OUTER JOIN
+
+10.Subqueries
+
+• Subqueries used in • Filtering data
+
+• Aggregating data
+
+• Joining tables
+
+• Correlated Subqueries

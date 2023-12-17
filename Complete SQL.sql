2ecd1454182a311613f634dc3ef6db4cb85670a0
Gagan Yadav <112801109+Gagan-Yadav@users.noreply.github.com>
2023-12-17 13:48:42 +0530

    Initial commit

---
+++ Complete SQL.sql
@@ -0,0 +1,57 @@
+1. Intro to SQL
+
+• Definition
+
+• Purpose • Relational DBs
+
+• DBMS
+
+2.Basic SQL Syntax
+
+• SELECT
+
+• FROM
+
+• WHERE
+
+• ORDER BY
+
+• GROUP BY
+
+3. Data Types
+
+• Integer
+
+• Floating-Point
+
+• Character
+
+• Date
+
+• VARCHAR
+
+• TEXT
+
+• BLOB
+
+• BOOLEAN
+
+4.Sub languages
+
+• DML
+
+• DDL
+
+DQL
+
+• DCL
+
+• TCL
+
+5. Data Manipulation
+
+• INSERT
+
+• UPDATE
+
+• DELETE
